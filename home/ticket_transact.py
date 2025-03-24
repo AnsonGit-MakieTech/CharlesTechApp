8f8a755f637adb19607cd0cabd3c880d53a0e230
AnsonGit-MakieTech <techmakie@gmail.com>
2025-03-24 19:00:36 +0800

    start accoutn screen design

--- home/ticket_transact.py
+++ home/ticket_transact.py
@@ -1,3 +1,4 @@
+from kivy.uix.accordion import ObjectProperty
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 from kivy.metrics import dp,sp 
@@ -8,10 +9,50 @@ from types import MethodType  # ✅ Import MethodType
  
 from kivy.animation import Animation
   
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout 
 import os
 from variables import *
 from .home_component import *
+
+
+class AccountInfoLayout(MDBoxLayout):
+    icon_image : str = StringProperty('')
+    account_info : str = StringProperty("......")
+    click_event : object = ObjectProperty(None)
+    
+    info_label : BoxLayout = ObjectProperty(None)
+    
+    def setup(self, icon_image = None, account_info = None , click_event = None):
+        if (icon_image):
+            self.icon_image = icon_image
+        if (account_info): 
+            
+            if (click_event):  
+                def on_touch_down(label, touch):
+                    """ Detect touch inside the image """
+                    if label.collide_point(*touch.pos):
+                        click_event()
+                        return True  # ✅ Stops event propagation if needed
+                    return super(label.__class__, label).on_touch_down(touch)  # ✅ Call original method 
+                self.info_label.on_touch_down = MethodType(on_touch_down, self.info_label)
+                account_info = f"[u]{account_info}[/u]"
+                
+            self.account_info = account_info
+
+class AccountNumberNameLayout(BoxLayout):
+    account_image : str = StringProperty('')
+
+    def setup_image(self, account_image = None): 
+        # ✅ Set the background image path BEFORE adding widgets
+        if (account_image):
+            
+            return
+        parent_dir = os.path.dirname(os.path.dirname(__file__))
+        self.account_image = os.path.join(parent_dir, 'assets', 'account_image.png') 
+        print("account_image : ", self.account_image)
+         
 
 
 class TicketTransactionScreeen(Screen):
@@ -23,6 +64,17 @@ class TicketTransactionScreeen(Screen):
     back_text: Button = ObjectProperty(None)
     go_back_screen_font_size = NumericProperty(0)  # ✅ Initialize with 0 (Will update later)
     go_back_icon : str = StringProperty('')
+    account_name : AccountNumberNameLayout = ObjectProperty(None)
+    
+    account_name_info : AccountInfoLayout = ObjectProperty(None)
+    account_email : AccountInfoLayout = ObjectProperty(None) 
+    account_loc1 : AccountInfoLayout = ObjectProperty(None)
+    account_loc2 : AccountInfoLayout = ObjectProperty(None)
+    account_loc3 : AccountInfoLayout = ObjectProperty(None)
+    account_phone1 : AccountInfoLayout = ObjectProperty(None)
+    account_phone2 : AccountInfoLayout = ObjectProperty(None)
+    account_phone3 : AccountInfoLayout = ObjectProperty(None)
+    
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -30,6 +82,7 @@ class TicketTransactionScreeen(Screen):
         parent_dir = os.path.dirname(os.path.dirname(__file__)) 
         
         self.go_back_icon = os.path.join(parent_dir, 'assets', 'go_back_icon.png')
+        print("go back icon", self.go_back_icon)
         self.bind(size=self.update_size)  # Bind window resize event
         
     
@@ -44,9 +97,16 @@ class TicketTransactionScreeen(Screen):
         self.go_back_screen_font_size = min(Window.width, Window.height) * 0.04
         print("update_size : ", self.text_input_font_size)
 
-
     def on_enter(self, *args):
         Animation(opacity=1, duration=0.5).start(self)
+        self.account_name_info.setup(icon_image='account-box' , account_info="Tech Makie Catamora")
+        self.account_email.setup(icon_image='email' , account_info="techmakie@gmail.com")
+        self.account_loc1.setup(icon_image='google-maps' , account_info="123 Main St, San Francisco, CA" , click_event=self.go_back)
+        self.account_loc2.setup( account_info="456 Elm St, San Francisco, CA", click_event=self.go_back)
+        self.account_loc3.setup( account_info="789 Oak St, San Francisco, CA", click_event=self.go_back)
+        self.account_phone1.setup(icon_image='phone' , account_info="123-456-7890")
+        self.account_phone2.setup(account_info="987-654-3210")
+        self.account_phone3.setup(account_info="098-765-4321")
         
         self.back_text.on_press=self.go_back
             
@@ -59,6 +119,9 @@ class TicketTransactionScreeen(Screen):
 
         # ✅ Bind on_touch_down correctly
         self.back_image.on_touch_down = MethodType(on_touch_down, self.back_image)
+        
+        self.account_name.setup_image()  # ✅ Set the background image path before adding widgets
+        
         
         return super().on_enter(*args)
     
